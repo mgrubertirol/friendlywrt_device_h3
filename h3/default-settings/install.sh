79e44aba9d46cffc15d1414b3541690aab8413c2
Lawrence-Tang <tangzongsheng@gmail.com>
2019-10-14 17:01:04 +0800

    fixed the interface conflict issue between usb wifi and ap6212

--- h3/default-settings/install.sh
+++ h3/default-settings/install.sh
@@ -17,6 +17,14 @@ function setup_openwrt()
     # disable uci_rollback for change ap to sta with 192.168.2.1
     sed -i 's/uci_apply(true)/uci_apply(false)/' ./usr/lib/lua/luci/view/footer.htm
 
+    if [ -f ./etc/modules.d/brcmfmac ]; then
+    	mv ./etc/modules.d/brcmfmac ./etc/modules.d/10-brcmfmac
+    fi
+
+    mkdir -p ./boot
+    chmod 0755 ./boot
+    echo '/dev/mmcblk0p1    /boot vfat defaults 0 0' > ./etc/fstab
+
     # rom version
     DATE=$(date +%Y%m%d)
     echo "${DATE}" >./etc/rom-version
