bf2290ba1965ffddcac78cbfec4011079630e471
Lawrence-Tang <tangzongsheng@gmail.com>
2019-11-09 17:27:54 +0800

    add config file for nanopi-r1s-h3

---
+++ h3/nanopi_r1s_h3.mk
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. device/friendlyelec/h3/nanopi_r1.mk
+TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-H1S-H3_armhf_sd.img
+TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-H1S-H3_armhf_eflasher.img
