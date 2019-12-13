0e6946f8df07a53983c5e33a7fc163a44c39bd87
Lawrence-Tang <tangzongsheng@gmail.com>
2019-12-13 16:48:19 +0800

    fix r1s filename

--- h3/nanopi_r1s_h3.mk
+++ h3/nanopi_r1s_h3.mk
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . device/friendlyelec/h3/nanopi_r1.mk
-TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-H1S-H3_armhf_sd.img
-TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-H1S-H3_armhf_eflasher.img
+TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R1S-H3_armhf_sd.img
+TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R1S-H3_armhf_eflasher.img
